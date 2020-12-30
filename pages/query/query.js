6f0775548b59cf8cb61f3ba561441f541905172f
Zhemin <zhangzhemin0929@163.com>
2020-12-30 13:28:01 +0800

    20201230

--- pages/query/query.js
+++ pages/query/query.js
@@ -9,21 +9,21 @@ Page({
     recordEndTime: "",
     begin: "2000-06-01",
     end: "2100-06-01",
-    name: ""
+    name: "",
   },
 
-  inputComName: function(e) {
+  inputComName: function (e) {
     var _this = this;
     _this.setData({
-      companyName: e.detail.value
+      companyName: e.detail.value,
     });
   },
-  formSubmit: function(e) {
+  formSubmit: function (e) {
     let loginFlag = wx.getStorageSync("loginFlag");
     if (!loginFlag) {
       wx.showToast({
         title: "您还未登录，请先登录",
-        icon: "none"
+        icon: "none",
       });
       return;
     }
@@ -35,11 +35,11 @@ Page({
     // var authET = util.formatToDate(this.data.authEndTime) / 1000 + 14400;
     var recordST = util.formatToDate(this.data.recordStartTime) / 1000 + 14400;
     var recordET = util.formatToDate(this.data.recordEndTime) / 1000 + 14400;
-    
+
     if (recordST > recordET) {
       wx.showToast({
         title: "时间错误",
-        icon: "none"
+        icon: "none",
       });
       return;
     }
@@ -56,65 +56,69 @@ Page({
         // "authEndTime": authET,
         startTime: recordST,
         endTime: recordET,
-        timeStamp: dateTemp ,// TODO: 发起请求的时间，unix时间戳
-        attachment:0,
+        timeStamp: dateTemp, // TODO: 发起请求的时间，unix时间戳
+        attachment: 0,
       },
       method: "POST",
-      success: function(e) {
+      success: function (e) {
         console.log(e);
         wx.hideLoading();
-        if (e.statusCode == 200 && type== "form") {
+        if (e.statusCode == 200 && type == "form") {
           wx.setStorageSync("table", e.data);
           wx.navigateTo({
-            url: "../table/table"
+            url: "../table/table",
           });
-        } 
-        else if (e.statusCode == 200 && type == "bill"){
-          if(e.data.flowAccount.length==0){
+        } else if (e.statusCode == 200 && type == "bill") {
+          if (e.data.flowAccount.length == 0) {
             wx.showToast({
-              title: '所选时间无流水账记录',
-              icon:'none',
-            })
-          }
-          else{
+              title: "所选时间无流水账记录",
+              icon: "none",
+            });
+          } else {
             wx.setStorageSync("cashFlow", e.data);
             wx.navigateTo({
-              url: "../BillInfo/Billinfo" + "?recordStartTime=" + recordST + "&recordEndTime=" + recordET + "&date=" + dateTemp,
+              url:
+                "../BillInfo/Billinfo" +
+                "?recordStartTime=" +
+                recordST +
+                "&recordEndTime=" +
+                recordET +
+                "&date=" +
+                dateTemp,
             });
           }
-        }
-        else {
+        } else {
           wx.showToast({
             title: e.data,
-            icon: "none"
+            icon: "none",
           });
         }
       },
-      fail: function(e) {
+      fail: function (e) {
         wx.hideLoading();
         wx.showToast({
           title: "查询失败",
-          icon: "none"
+          icon: "none",
         });
-      }
+      },
     });
   },
 
-  bindDateChange: function(e) {
+  bindDateChange: function (e) {
     if (e.target.id == "recordStartTime") {
       this.setData({
-        recordStartTime: e.detail.value
+        recordStartTime: e.detail.value,
       });
     } else if (e.target.id == "recordEndTime") {
       this.setData({
-        recordEndTime: e.detail.value
+        recordEndTime: e.detail.value,
       });
     }
   },
-  onReady: function() {
+  onReady: function () {
     this.setData({
       recordStartTime: util.formatTime(new Date(), "yyyy-MM-dd"),
-      recordEndTime: util.formatTime(new Date(), "yyyy-MM-dd")
+      recordEndTime: util.formatTime(new Date(), "yyyy-MM-dd"),
     });
-  }
+  },
 });
